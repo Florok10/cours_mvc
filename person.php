685392a594a15b5877f2724231c1c9d309eba332
Valentin <vf59165@hotmail.com>
2021-05-27 22:51:53 +0200

    22:51

--- person.php
+++ person.php
@@ -1,22 +1,38 @@
 <?php 
 
 class User {
-    private $_nom;
+    var $_nom;
     private $_prenom;
     private $_age;
     private $_email;
+    private $_passwordUser
 
-    public function __construct($_nom) {
-        echo $_nom . " ";
+    public function __construct($nom, $prenom, $age, $email, $passwordUser) {
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_age = $age;
+        $this->_email = $mail;
+        $this->_passwordUser = $passwordUser;
+    }
+
+    public function SetNom($nom) {
+        $this->_nom = $nom;
+    }
+    public function SetPrenom($prenom) {
+        $this->_prenom = $prenom;
+    }
+    public function SetAge($age) {
+        $this->_age = $age;
+    }
+    public function SetMail($mail) {
+        $this->_mail = $mail;
+    }
+    public function SetPassword_user($password_user) {
+        $this->_password_user = $password_user;
     }
 }
 
-$valentin = new User('Valentin');
-$mourad = new User('Mourad');
-$omar = new User('Omar');
-$steven = new User('Steven');
-$dany = new User('Dany');
-$emilie = new User('Emilie');
-$mickael = new User('Mickael');
-$baptiste = new User('Baptiste');
-$marjorie = new User('Marjorie');
+$valentin = new User('Fekrach', 'Valentin', 22, 'vf59165@hotmail.com', 'a1b2c3');
+
+var_dump($valentin->$nom->$prenom->$age->$email->$passwordUser);
+
