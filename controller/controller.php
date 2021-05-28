910678c3233b876dd7ff62fa8397b53a13e395b5
Valentin <vf59165@hotmail.com>
2021-05-28 10:17:45 +0200

    tentative_de_debugage

--- controller/controller.php
+++ controller/controller.php
@@ -1,6 +1,6 @@
 <?php
 
-require ('pdo.php');
+require ('../model/pdo.php');
 
 if (isset( $_POST['submit'] ) ) {
   $nom = $_POST['nom']; 
@@ -12,18 +12,19 @@ if (isset( $_POST['submit'] ) ) {
 
 try {
   $bdd = new PDO($dsn);
-  $preparation_requete = $dsn->prepare("INSERT INTO user (nom, prenom, age, email, passwordUser)
-   VALUES (:nom, :prenom, :age, :email, :passwordUser)");
+  $preparation_requete = $dsn->prepare
+  ('INSERT INTO user (nom, prenom, age, email, passwordUser)
+  VALUES (:nom, :prenom, :age, :email, :passwordUser)');
 }
   catch (Exception $e) {
     die('Erreur : ' . $e->getMessage())
   }
 
-  $preparation_requete -> bindParam(':nom', $nom);
-  $preparation_requete -> bindParam(':prenom', $prenom);
-  $preparation_requete -> bindParam(':age', $age);
-  $preparation_requete -> bindParam(':email', $email);
-  $preparation_requete -> bindParam(':passwordUser', $passwordUser);
+  $preparation_requete->bindParam(':nom', $nom);
+  $preparation_requete->bindParam(':prenom', $prenom);
+  $preparation_requete->bindParam(':age', $age);
+  $preparation_requete->bindParam(':email', $email);
+  $preparation_requete->bindParam(':passwordUser', $passwordUser);
 
-  $preparation_requete -> execute();
+  $preparation_requete->execute();
 ?>
---
+++ model/pdo.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$port = "80";
+$dbname = "cours_mvc";
+$charset = "UTF8";
+$user = "phpmyadmin";
+$password = "root";
+
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset", $user, $password;
+?>
